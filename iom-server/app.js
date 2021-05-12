b66ed735fbcd9fbe92503f20bd5b8046fdd2a3fd
mritunjay7497 <mkumarmani97@gmail.com>
2021-05-12 15:18:02 +0530

    initial commit, sign-up and login functionalities added

--- iom-server/app.js
+++ iom-server/app.js
@@ -1,41 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const home = require('./router/home');
+const owner = require('./router/owner');
+const signin = require('./router/ownerLogin');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Route handler middleware
+app.use('/home',home);
+app.use('/api/register',owner);
+app.use('/login',signin);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// get port number from environment variable
+const PORT = process.env.PORT || 1111
+
+// start the server
+app.listen(PORT,() => {
+    console.log(`server started on http://localhost:${PORT}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
---
+++ iom-server/router/ownerLogin.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Joi = require('joi');
+const ownerAuth = require('../models/ownerLogin');
+const bodyParser = require('body-parser');
+
+const loginRoute = express.Router();
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// Route to authenticate owner
+
+loginRoute.post('/',jsonParser,(req,res) => {
+
+    // match credentials from DB
+    const validCreds = ownerAuth(req.body.email,req.body.password)
+        .then((data) => res.header('x-auth-token',data.token).send("Log-in successful."))
+        .catch((err) => res.send(err));
+
+});
+
+module.exports = loginRoute;
